58485c6d3b1f42bcdc5bbbfe1678f3b7fb80632b
unknown <mohzulkiflikatili@gmail.com>
2021-06-21 18:58:01 +0800

    loader diperbaiki pake beforunload

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -46,7 +46,17 @@
 </head>
 
 <body class="bg-gray-100 f-opensans font-sans antialiased">
-    @include('layouts.loaderhalaman')
+    <!-- loader halaman -->
+    <div id="loading-image" class="pembungkus-loader min-h-screen fixed w-full z-10 flex flex-col space-y-3 justify-center items-center bg-gray-100 opacity-80">
+        <div class="loader bg-white p-5 rounded-full flex space-x-3">
+            <div class="w-5 h-5 bg-green-400 rounded-full animate-bounce"></div>
+            <div class="w-5 h-5 bg-green-400 rounded-full animate-bounce"></div>
+            <div class="w-5 h-5 bg-green-400 rounded-full animate-bounce"></div>
+        </div>
+        <span class="animate-pulse">Loading.. </span>
+    </div>
+    <!-- loader halaman -->
+
 
 
     {{-- nav atas --}}
@@ -85,6 +95,28 @@
 {{-- PWA HOMESCREEN --}}
 {{-- @include('layouts.scriptAddToHomeScreen') --}}
 
+
+
+
+<!-- loader halaman -->
+<script>
+    // const pemb = document.querySelector('.pembungkus-loader');
+    // window.onload = function(event) {
+    //   pemb.remove();
+    // };
+
+
+    $(function () {
+        // page is loaded, it is safe to hide loading animation
+        $('#loading-image').hide();
+
+        $(window).on('beforeunload', function () {
+            // user has triggered a navigation, show the loading animation
+            $('#loading-image').show();
+        });
+    });
+</script>
+<!-- loader halaman -->
 
 
 </html>
--- resources/views/layouts/loaderhalaman.blade.php
+++ resources/views/layouts/loaderhalaman.blade.php
@@ -1,5 +1,5 @@
 <!-- loader halaman -->
-<div class="pembungkus-loader min-h-screen fixed w-full z-10 flex flex-col space-y-3 justify-center items-center bg-gray-100 opacity-80">
+<div id="loading-image" class="pembungkus-loader min-h-screen fixed w-full z-10 flex flex-col space-y-3 justify-center items-center bg-gray-100 opacity-80">
     <div class="loader bg-white p-5 rounded-full flex space-x-3">
         <div class="w-5 h-5 bg-green-400 rounded-full animate-bounce"></div>
         <div class="w-5 h-5 bg-green-400 rounded-full animate-bounce"></div>
@@ -12,5 +12,18 @@
     window.onload = function(event) {
       pemb.remove();
     };
+
+
+    // $(function () {
+    //     // page is loaded, it is safe to hide loading animation
+    //     $('#loading-bg').hide();
+    //     $('#loading-image').hide();
+
+    //     $(window).on('beforeunload', function () {
+    //         // user has triggered a navigation, show the loading animation
+    //         $('#loading-bg').show();
+    //         $('#loading-image').show();
+    //     });
+    // });
 </script>
 <!-- loader halaman -->
