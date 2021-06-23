40eccc85a726251e70c6b8dfa392e45a61836bcd
unknown <mohzulkiflikatili@gmail.com>
2021-06-23 13:32:21 +0800

    percobaan bug drop 3

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -39,7 +39,7 @@
 
 
 
-    {{-- <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.8.2/dist/alpine.min.js" defer></script> --}}
+    <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.8.2/dist/alpine.min.js" defer></script>
 
     {{$stylehalaman}}
 
