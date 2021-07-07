de9bb2317d7a475beb9e92135e3d891f3eb972f8
IqbalSalim <iqbalbinsalim@gmail.com>
2021-07-07 19:20:06 +0800

    perbaikan tampilan

--- resources/views/livewire/dropupkalender.blade.php
+++ resources/views/livewire/dropupkalender.blade.php
@@ -9,7 +9,7 @@
     <div wire:ignore.self :class="{ 'translate-y-full' : !dropUpOpen , }" class="fixed inset-x-0 bottom-0 z-40
         transform transition translate-y-full duration-200 ease-in-out
         min-w-screen bg-white pb-10 rounded-t-2xl shadow border-t-2 border-gray-200 opacity-95
-        flex flex-col space-y-2  justify-between h-screen overflow-auto"{{-- h-3/4 --}}
+        flex flex-col space-y-2  justify-between h-4/5 overflow-auto"{{-- h-3/4 --}}
         >
 
 
