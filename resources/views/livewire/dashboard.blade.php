23ca13ee767a3013d9ac9e05036dc2636bd8d228
Moh Zulkifli Katili <mohzulkiflikatili@Moh-Zulkifli.local>
2021-12-26 11:37:08 +0800

    grid mas abil done

--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -62,91 +62,93 @@
             </div>
             
             @foreach ($item->anggotaunits as $ag)
-                <div class="min-w-max w-full flex justify-around align-items-center space-x-2 my-2">
-                    <span>{{$ag->nama_singkat_dua}}</span>
-                    <div class="flex align-items-center space-x-0">
-                        @for ($i = 1; $i <= 12; $i++)
+                <div class="min-w-max w-full grid lg:grid-cols-4 lg:px-4 justify-around align-items-center space-x-2 my-2">
+                    <span class="text-right">{{$ag->nama_semi_singkat}}</span>
+                    <div class="col-span-3">
+                        <div class="grid grid-cols-12 align-items-center space-x-0">
+                            @for ($i = 1; $i <= 12; $i++)
 
-                            @php
-                                $jumlahHari=$this->getJumlahWeekdayDalamBulan(
-                                    $posisi->year,
-                                    $i
-                                );
-                            
-                                $iniTot=$ag->getTugasDalamBulan($i,$posisi->year);
-                                $tot=0;
-                                if($iniTot->isNotEmpty())
-                                {
-                                    $iniTot=$this->pecahHariBulan($iniTot,FALSE,$i,$posisi->year);
-                                    foreach ($iniTot as $t) 
+                                @php
+                                    $jumlahHari=$this->getJumlahWeekdayDalamBulan(
+                                        $posisi->year,
+                                        $i
+                                    );
+                                
+                                    $iniTot=$ag->getTugasDalamBulan($i,$posisi->year);
+                                    $tot=0;
+                                    if($iniTot->isNotEmpty())
                                     {
-                                        $tot=$tot+$t;
+                                        $iniTot=$this->pecahHariBulan($iniTot,FALSE,$i,$posisi->year);
+                                        foreach ($iniTot as $t) 
+                                        {
+                                            $tot=$tot+$t;
+                                        }
                                     }
-                                }
-                                $tot=$tot+($jumlahHari*4);//menambahkan dengan tugas rutin
-                                $max=$jumlahHari*12;
-                            @endphp
+                                    $tot=$tot+($jumlahHari*4);//menambahkan dengan tugas rutin
+                                    $max=$jumlahHari*12;
+                                @endphp
 
-                            {{-- <div>
-                                <span style="font-size: 10px" class="
-                                    rounded-full w-4 h-4 flex items-center justify-center font-bold 
-                                    @if ($tot==0)
-                                    text-gray-400 
-                                    @elseif($tot <= ($max*0.5))
-                                    text-gray-500 bg-green-200 
-                                    @elseif($tot <= ($max*0.75))
-                                    text-gray-200 bg-yellow-400
-                                    @else
-                                    text-gray-200 bg-red-400 
-                                    @endif
-                                    ">
-                                    {{$i}}
-                                </span>
-                                <sub class="diagonal-fractions">
-                                    {{$tot}}
-                                    /{{$max}}
-                                </sub> 
-                            </div> --}}
-
-                            <div class="px-0.5">
-                                <div class="flex flex-col">
-                                    <span class="text-center text-xs">
+                                {{-- <div>
+                                    <span style="font-size: 10px" class="
+                                        rounded-full w-4 h-4 flex items-center justify-center font-bold 
+                                        @if ($tot==0)
+                                        text-gray-400 
+                                        @elseif($tot <= ($max*0.5))
+                                        text-gray-500 bg-green-200 
+                                        @elseif($tot <= ($max*0.75))
+                                        text-gray-200 bg-yellow-400
+                                        @else
+                                        text-gray-200 bg-red-400 
+                                        @endif
+                                        ">
                                         {{$i}}
-                                        <span class="material-icons 
-                                                    text-gray-200
-                                                    " style="font-size: 9px">
-                                            circle
-                                        </span>
                                     </span>
-                                    <div class="flex-1">
-                            
-                                        <div class="flex flex-col flex-wrap">
-                                            <span class="
-                                                @if ($tot==0)
-                                                text-gray-400 
-                                                @elseif($tot <= ($max*0.5))
-                                                text-gray-500 bg-green-200 
-                                                @elseif($tot <= ($max*0.75))
-                                                text-gray-600 bg-yellow-200
-                                                @else
-                                                text-gray-400 bg-red-200 
-                                                @endif
-                                                    px-0.5 text-center rounded-sm font-bold"
-                                                style="font-size: 11px;">
-                                                {{$tot}} <br>
-                                                <sub>{{$max}}</sub>
-                                                
+                                    <sub class="diagonal-fractions">
+                                        {{$tot}}
+                                        /{{$max}}
+                                    </sub> 
+                                </div> --}}
+
+                                <div class="px-0.5">
+                                    <div class="flex flex-col">
+                                        <span class="text-center text-xs">
+                                            {{$i}}
+                                            <span class="material-icons 
+                                                        text-gray-200
+                                                        " style="font-size: 9px">
+                                                circle
                                             </span>
+                                        </span>
+                                        <div class="flex-1">
+                                
+                                            <div class="flex flex-col flex-wrap">
+                                                <span class="
+                                                    @if ($tot==0)
+                                                    text-gray-400 
+                                                    @elseif($tot <= ($max*0.5))
+                                                    text-gray-500 bg-green-200 
+                                                    @elseif($tot <= ($max*0.75))
+                                                    text-gray-600 bg-yellow-200
+                                                    @else
+                                                    text-gray-400 bg-red-200 
+                                                    @endif
+                                                        px-0.5 text-center rounded-sm font-bold"
+                                                    style="font-size: 11px;">
+                                                    {{$tot}} <br>
+                                                    <sub>{{$max}}</sub>
+                                                    
+                                                </span>
+                                            </div>
+                                
                                         </div>
-                            
                                     </div>
                                 </div>
-                            </div>
-                            
-                        @endfor
-                        {{-- <span class="material-icons text-base">
-                            people
-                        </span> --}}
+                                
+                            @endfor
+                            {{-- <span class="material-icons text-base">
+                                people
+                            </span> --}}
+                        </div>
                     </div>
                 </div>
             @endforeach
@@ -163,7 +165,7 @@
         <br>
         {{-- ================================================================ --}}
 
-        <div class="mt-6 flex justify-between items-center">
+        <div class="mt-6 flex flex-col lg:flex-row justify-between lg:items-center">
             <div class="font-bold text-gray-500 f-robotocon text-xl">
                 Statistik
             </div>
