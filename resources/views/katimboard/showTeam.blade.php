e05bb3f32329c8d5026031acb885e181b51aa1fd
kikiraihan <mohzulkiflikatili@gmail.com>
2022-07-04 11:41:37 +0800

    cek

--- resources/views/katimboard/showTeam.blade.php
+++ resources/views/katimboard/showTeam.blade.php
@@ -1,6 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         @include('layouts.navigationback',['warna'=>'bg-white','kata'=>'Tentang Tim','link_balik'=>$link])
+        {{$link}}
     </x-slot>
 
     <x-slot name="footer">
